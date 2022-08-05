4cc54206479181c3d7958ebf0ec3c44ae9b4ec41
SparshJain <jainsparsh17@gmail.com>
2022-08-05 19:08:40 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from "react";
 import "./index.css";
 import App from "./App";
-import { hydrate, render } from "react-dom";
+import ReactDOM from 'react-dom';
 
-const APP = (
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root');
 );
-
-const rootElement = document.getElementById("root");
-if (rootElement.hasChildNodes()) {
-  hydrate(APP, rootElement);
-} else {
-  render(APP, rootElement);
-}
